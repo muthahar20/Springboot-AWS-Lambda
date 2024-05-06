0a6deba9b4714caac3949975b6dd8436469e5038
muthahar20@gmail.com <muthahar20@gmail.com>
2024-05-06 13:02:05 -0400

    sb-mysql-junit5 project added

---
+++ sb-mysql-junit5/src/main/java/com/mtr/sbmysqljunit5/SbMysqlJunit5Application.java
@@ -0,0 +1,13 @@
+package com.mtr.sbmysqljunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMysqlJunit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMysqlJunit5Application.class, args);
+	}
+
+}
---
+++ sb-mysql-junit5/src/test/java/com/mtr/sbmysqljunit5/SbMysqlJunit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtr.sbmysqljunit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMysqlJunit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
